107a22f64c6c79d086e576d62f34a14bb0c5b9b1
Camilo Ordoñez <cronox20@gmail.com>
2023-07-27 09:13:45 -0500

    adding first userReducer, needs improve

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,20 @@
+import { useSelector } from 'react-redux';
+import { UserState } from '../../data/store/store';
 import { User } from '../../types';
 import '../../App.css';
 import "./Card.css"
 
-interface CardProps {
-    content: User
-}
 
-export default function Card({ content }: CardProps) {
+export default function Card() {
+
+    const user: User = useSelector((state: UserState) => state.user);
+
     return (
         <div className="mt-48 mb-96 mx-auto card">
-            <img className="mt-0 mb-0 photo" src={content.photo} alt={content.name} />
-            <p className="mt-18 mb-0 resume">{content.resume}</p>
-            <h5 className="mt-36 mb-0 name">{content.name}</h5>
-            <p className="mt-12 mb-0 company">{content.company.role} at <br/> {content.company.name}</p>
+            <img className="mt-0 mb-0 photo" src={user.photo} alt={user.name} />
+            <p className="mt-18 mb-0 resume">{user.resume}</p>
+            <h5 className="mt-36 mb-0 name">{user.name}</h5>
+            <p className="mt-12 mb-0 company">{user.company.role} at <br/> {user.company.name}</p>
         </div>
     )
-} 
+}
--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../data/reducers/userSlice";
 import Card from "../Card/Card";
 import '../../App.css';
 import './TopSection.css';
 import { User } from "../../types";
 
+
 export default function TopSection() {
 
-    const [data, setData] = useState([]);
-    const [description, setDescription] = useState('');
+    // const [data, setData] = useState([]);
+    // const [description, setDescription] = useState('');
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch('http://localhost:4000/community');
+    //         const json = await response.json();
+    //         setData(json.data);
+    //         setDescription(json.description);
+    //     };
+    //     fetchData();
+    // }, []);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch('http://localhost:4000/community');
-            const json = await response.json();
-            setData(json.data);
-            setDescription(json.description);
+            const response: Response = await fetch('http://localhost:4000/community');
+            const { data }: { data: User[] } = await response.json();
+            data.forEach((user: User) => dispatch(setUser(user)));
         };
         fetchData();
-    }, []);
+    }, [dispatch]);
+
 
     const [displayContent, setDisplayContent] = useState(false);
 
@@ -27,9 +42,12 @@ export default function TopSection() {
             <button className={displayContent ? 'mt-24 mb-0' : 'mt-24 mb-96'} onClick={() => setDisplayContent(!displayContent)}>{displayContent ? 'Hide' : 'Show'} section</button>
             {displayContent && (
                 <>
-                    <h4 className='mt-24 mb-0 mx-30 description'>{description}</h4>
+                    <h4 className='mt-24 mb-0 mx-30 description'>We're proud of our products, and we're really excited when we get feedback from our users.</h4>
                     <div className='cards-container'>
-                        {data.map((user: User) => <Card key={user.name} content={user}/>)}
+                        {/* {data.map((user: User) => <Card key={user.name} />)} */}
+                        <Card />
+                        <Card />
+                        <Card />
                     </div>
                 </>
             )}
